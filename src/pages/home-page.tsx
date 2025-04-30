fbe3458f346dcbe90d007a4c71a735a10ff933ca
Dmytro <gudzoniya111@gmail.com>
2025-04-30 16:29:07 +0300

    add p for home page

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,7 +1,10 @@
 export const HomePage = () => {
-    return (
-        <section>
-            <h1>Home Page</h1>
-        </section>
-    )
-}
+  return (
+    <section>
+      <h1>Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, nihil.
+      </p>
+    </section>
+  );
+};
