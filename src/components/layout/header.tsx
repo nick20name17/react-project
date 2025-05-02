2266ce0cfdb26137e9c50f1791dc89ebca2aebee
Dmytro <gudzoniya111@gmail.com>
2025-05-02 16:21:10 +0300

    add theme provider

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,13 +1,15 @@
-import { Button } from '@/components/ui/button'
-import { Atom } from 'lucide-react'
+import { Button } from "@/components/ui/button";
+import { Atom } from "lucide-react";
+import { ModeToggle } from "../mode-toggle";
 
 export const Header = () => {
-    return (
-        <header className="rounded-full h-14 sticky top-4 flex items-center justify-center  border bg-gray-200/10  mx-auto max-w-3xl   bg-clip-padding backdrop-filter backdrop-blur-sm  ">
-            <Button size="sm">
-                <Atom />
-                <span>Header</span>
-            </Button>
-        </header>
-    )
-}
+  return (
+    <header className="rounded-full h-14 sticky top-4 flex items-center justify-center  border bg-gray-200/10  mx-auto max-w-3xl   bg-clip-padding backdrop-filter backdrop-blur-sm  ">
+      <Button size="sm">
+        <Atom />
+        <span>Header</span>
+      </Button>
+      <ModeToggle />
+    </header>
+  );
+};
